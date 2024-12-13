4097cff743631cbd03429256be262699457de18a
M1ckh1ck <85888897+M1ckh1ck@users.noreply.github.com>
2024-12-13 10:38:13 +0000

    Portfolio

--- equity/equity_statistics.py
+++ equity/equity_statistics.py
@@ -1,15 +1,36 @@
 from equity_class import Equity
 
 portfolio = {}
-
 finished = False 
-while not finished:
-    ticker = input("""Please enter ticker symbol or "quit" to finish: """).upper()
-    if ticker == "QUIT":
-        finished = True
-    else:
-        holding = Equity(ticker)
-        symbol = holding.symbol
-        portfolio[symbol] = holding
 
-print(portfolio)
+while not finished:
+    ticker = input("""Please enter ticker symbol? """).upper().strip()
+    shares = int(input("How many shares were purchased? "))
+    
+    holding = Equity(ticker)
+    symbol = holding.symbol
+    info = {
+            "equity": holding,
+            "symbol": symbol,
+            "shares": shares,
+        }
+    portfolio[symbol] = info
+
+    another = input(
+            "Would you like to add another stock? (Y/N): "
+            ).upper().strip()
+    
+    if another == "N":
+        finished = True
+
+portfolio_value = 0
+
+for stock in portfolio:
+    shares = portfolio[stock]["shares"]
+    equity = portfolio[stock]["equity"]
+    price = equity.adj_close.iloc[-1]
+    portfolio_value += (shares * price)
+
+print(portfolio_value)
+
+
